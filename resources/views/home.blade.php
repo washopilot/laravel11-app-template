fe6ad0a3008469b5ec84c62c418a7ca707a8de8f
Fernando Chicaiza <washopilot@yahoo.com>
2025-03-22 19:32:53 +0000

    blade components

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document MF</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
+<x-layout>
+    <h1>Hello from Home Page</h1>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
